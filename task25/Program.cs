54eed1f81aadcfe5590719ce4d1bc0d7b3b71100
Alexander <firefighter-911@rambler.ru>
2023-04-25 12:05:55 +0300

    Решение 25 задачи

---
+++ task25/Program.cs
@@ -0,0 +1,49 @@
+﻿//Напишите цикл, который на вход принимает числа (А и В) и
+//возводит число А в натуральную степень.
+
+Console.Clear();
+
+int numA = GetFirstNumberFromUser("Введите число A: ", "Ошибка ввода!");
+int numB = GetSecondNumberFromUser("Введите число B: ", "Ошибка ввода!");
+
+double calculateResult = CalculateNumber(numA, numB);
+
+PrintResult(numA,numB,calculateResult);
+
+void PrintResult(int numA, int numB, double calculateResult)
+{
+    Console.WriteLine($"{numA},{numB} -> {calculateResult}");
+}
+
+
+int GetFirstNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int GetSecondNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+double CalculateNumber(int numberA, int numberB)
+{
+    double result = Math.Pow(numberA, numberB);
+    return result;
+}
+
+
